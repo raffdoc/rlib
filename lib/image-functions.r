598f2e7451d9d053f6de99ad4921f4969448439f
satoshi <stakahama@turtle-4.local>
2011-06-26 06:11:23 -0700

    ./push

---
+++ lib/image-functions.r
@@ -0,0 +1,73 @@
+find.nodes <- function(patterns,env) function(x) {
+  ## use with rapply(...,how="list")
+  tree <- as.list(substitute(x))
+  counter <- if(exists("counter",,env,inherits=FALSE)) get("counter",,env) else 1
+  for( var in names(patterns) )
+    if(!exists(var,,env,inherits=FALSE) &&
+       identical(tree[1:length(patterns[[var]])],
+                 patterns[[var]]))
+      assign(var,counter,,env)
+  assign("counter",counter+1,,env)
+}
+
+make.image.raster <- function(image.default) {
+  ## functions
+  isuniformp <- function(x,y)
+    anyDuplicated(x) + anyDuplicated(y) == 0
+  expandaxis <- function(x) {
+    dx <- diff(x)
+    seq(min(x),max(x),min(dx))
+  }
+  expandimage <- function(x,y,z) {
+    require(akima)
+    akima::interp(rep(x,ncol(z)),rep(y,each=nrow(z)),z,
+                  expandaxis(x),expandaxis(y))
+  }
+  ## code pieces
+  testuniform <- quote(if( !isuniformp(x,y) ) {
+    uniformgrid <- expandimage(x,y,z)
+    x <- uniformgrid$x
+    y <- uniformgrid$y
+    z <- uniformgrid$z
+    rm(uniformgrid)
+  })
+  rasterplot <- 
+    quote(rasterImage(with(list(z=t(structure(col[zi+1],dim=dim(zi)))),
+                           z[seq(nrow(z),1,-1),,drop=FALSE]),
+                      min(x),min(y),max(x),max(y),
+                      interpolate=interpolate))
+  ## function body as list
+  fnbody <- as.list(body(image.default))
+  ## find locations to insert/replace  
+  here <- new.env()
+  patterns <- list(firstafter=alist(`if`,!is.matrix(z)),
+                   imagecall=alist(.Internal,image(as.double(x),
+                     as.double(y),as.integer(zi),col)))
+  invisible(rapply(fnbody,find.nodes(patterns,here),how="list"))
+  first <- with(here,1:firstafter)
+  rest <- with(here,(firstafter+1):(imagecall-1))
+  ## modifications
+  formals(image.default)$interpolate <- TRUE
+  body(image.default) <-
+    as.call(c(fnbody[first],testuniform,fnbody[rest],rasterplot))
+  ## return
+  image.default
+}
+image.raster <- make.image.raster(graphics::image.default)
+
+make.filled.contour.nolegend <- function(filled.contour) {
+  fnbody <- as.list(body(filled.contour))
+  insert <- quote(if( add ) { axes <- FALSE } else {
+    plot.new()
+    plot.window(xlim, ylim, "", xaxs = xaxs, yaxs = yaxs, asp = asp)
+  })
+  here <- new.env()
+  patterns <- list(first=alist(`<-`,mar.orig),
+                   last=alist(`if`,!is.matrix(z) || nrow(z) <= 1L || ncol(z) <= 1L))
+  invisible(rapply(fnbody,find.nodes(patterns,here),how="list"))
+  body(filled.contour) <- as.call(c(head(fnbody,with(here,first-1)),
+                                    insert,
+                                    tail(fnbody,with(here,-(last-1)))))
+  filled.contour
+}
+filled.contour.nolegend <- make.filled.contour.nolegend(graphics::filled.contour)
