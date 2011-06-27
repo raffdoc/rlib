c28260e094543e036f5d2fe9def822c4674cae1c
satoshi <stakahama@turtle-4.local>
2011-06-27 04:50:38 -0700

    ./push

--- lib/image-functions.r
+++ lib/image-functions.r
@@ -14,25 +14,27 @@ make.image.raster <- function() {
   require(graphics)
   image.default <- graphics::image.default
   ## functions
-  isuniformp <- function(x,y)
-    anyDuplicated(x) + anyDuplicated(y) == 0
-  expandaxis <- function(x) {
-    dx <- diff(x)
-    seq(min(x),max(x),min(dx))
-  }
-  expandimage <- function(x,y,z) {
-    require(akima)
-    akima::interp(rep(x,ncol(z)),rep(y,each=nrow(z)),z,
-                  expandaxis(x),expandaxis(y))
-  }
   ## code pieces
-  testuniform <- quote(if( !isuniformp(x,y) ) {
-    uniformgrid <- expandimage(x,y,z)
-    x <- uniformgrid$x
-    y <- uniformgrid$y
-    z <- uniformgrid$z
-    rm(uniformgrid)
-  })
+  fndefs <-
+    alist(isuniformp <- function(x,y)
+          anyDuplicated(x) + anyDuplicated(y) == 0,
+          expandaxis <- function(x) {
+            dx <- diff(x)
+            seq(min(x),max(x),min(dx))
+          },
+          expandimage <- function(x,y,z) {
+            require(akima)
+            akima::interp(rep(x,ncol(z)),rep(y,each=nrow(z)),z,
+                          expandaxis(x),expandaxis(y))
+          })
+  testuniform <-
+    quote(if( !isuniformp(x,y) ) {
+      uniformgrid <- expandimage(x,y,z)
+      x <- uniformgrid$x
+      y <- uniformgrid$y
+      z <- uniformgrid$z
+      rm(uniformgrid)
+    })
   rasterplot <- 
     quote(rasterImage(with(list(z=t(structure(col[zi+1],dim=dim(zi)))),
                            z[seq(nrow(z),1,-1),,drop=FALSE]),
@@ -51,13 +53,13 @@ make.image.raster <- function() {
   ## modifications
   formals(image.default)$interpolate <- TRUE
   body(image.default) <-
-    as.call(c(fnbody[first],testuniform,fnbody[rest],rasterplot))
+    as.call(c(fnbody[first],fndefs,testuniform,fnbody[rest],rasterplot))
   ## return
   image.default
 }
 image.raster <- make.image.raster()
 
-make.filled.contour.nolegend <- function() {
+make.filled.contour.free <- function() {
   require(graphics)
   filled.contour <- graphics::filled.contour
   fnbody <- as.list(body(filled.contour))
@@ -69,13 +71,13 @@ make.filled.contour.nolegend <- function() {
   patterns <- list(first=alist(`<-`,mar.orig),
                    last=alist(`if`,!is.matrix(z) || nrow(z) <= 1L || ncol(z) <= 1L))
   invisible(rapply(fnbody,assign.nodenumbers(patterns,here),how="list"))
+  formals(filled.contour)$add <- FALSE
   body(filled.contour) <- as.call(c(head(fnbody,with(here,first-1)),
                                     insert,
                                     tail(fnbody,with(here,-(last-1)))))
   filled.contour
 }
-filled.contour.nolegend <- make.filled.contour.nolegend()
-
+filled.contour.free <- make.filled.contour.free()
 
 make.newimageplot <- function() {
   require(fields)
@@ -95,6 +97,38 @@ make.newimageplot <- function() {
   body(image.plot) <- as.call(c(fnbody,clipping))
   image.plot
 }
-
 newimageplot <- make.newimageplot()
 
+legend.colorbar <- function(...,axes=FALSE,ann=FALSE,
+                           horizontal=FALSE,axis.side=if(horizontal) 1 else 4,
+                           imagefn=graphics::image.default) {
+  ## to be called in conjunction with layout(), usually
+  args <- list(...)
+  nc <- length(if(is.null(args$col)) eval(formals(imagefn)$col) else args$col)
+  if( !is.null(args[["zlim"]]) ) {
+    ## zlim exists
+    y <- seq(min(zlim),max(zlim),,nc)
+  } else if( !is.null(args[["z"]]) ) {
+    ## z exists
+    y <- seq(min(z,na.rm=TRUE),max(z,na.rm=TRUE),,nc)
+  } else {
+    ## fail
+    stop("z or zlim must be specified")
+  }
+  x <- 1
+  z <- 1:nc
+  if( horizontal ) {
+    args$x <- y
+    args$y <- x
+  } else {
+    args$x <- x
+    args$y <- y
+  }
+  args$z <- structure(z,dim=c(length(args$x),length(args$y)))
+  do.call(imagefn,c(args,list(axes=axes,ann=ann)))
+  if( !is.na(axis.side) ) {
+    axis(axis.side)
+    box()
+  }
+  invisible()
+}
