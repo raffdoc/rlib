e97306dd0118294e555775261ce186613b84f510
satoshi <stakahama@turtle-4.local>
2011-06-26 23:08:42 -0700

    ./push

--- lib/image-functions.r
+++ lib/image-functions.r
@@ -1,4 +1,4 @@
-find.nodes <- function(patterns,env) function(x) {
+assign.nodenumbers <- function(patterns,env) function(x) {
   ## use with rapply(...,how="list")
   tree <- as.list(substitute(x))
   counter <- if(exists("counter",,env,inherits=FALSE)) get("counter",,env) else 1
@@ -10,7 +10,9 @@ find.nodes <- function(patterns,env) function(x) {
   assign("counter",counter+1,,env)
 }
 
-make.image.raster <- function(image.default) {
+make.image.raster <- function() {
+  require(graphics)
+  image.default <- graphics::image.default
   ## functions
   isuniformp <- function(x,y)
     anyDuplicated(x) + anyDuplicated(y) == 0
@@ -43,7 +45,7 @@ make.image.raster <- function(image.default) {
   patterns <- list(firstafter=alist(`if`,!is.matrix(z)),
                    imagecall=alist(.Internal,image(as.double(x),
                      as.double(y),as.integer(zi),col)))
-  invisible(rapply(fnbody,find.nodes(patterns,here),how="list"))
+  invisible(rapply(fnbody,assign.nodenumbers(patterns,here),how="list"))
   first <- with(here,1:firstafter)
   rest <- with(here,(firstafter+1):(imagecall-1))
   ## modifications
@@ -53,9 +55,11 @@ make.image.raster <- function(image.default) {
   ## return
   image.default
 }
-image.raster <- make.image.raster(graphics::image.default)
+image.raster <- make.image.raster()
 
-make.filled.contour.nolegend <- function(filled.contour) {
+make.filled.contour.nolegend <- function() {
+  require(graphics)
+  filled.contour <- graphics::filled.contour
   fnbody <- as.list(body(filled.contour))
   insert <- quote(if( add ) { axes <- FALSE } else {
     plot.new()
@@ -64,10 +68,33 @@ make.filled.contour.nolegend <- function(filled.contour) {
   here <- new.env()
   patterns <- list(first=alist(`<-`,mar.orig),
                    last=alist(`if`,!is.matrix(z) || nrow(z) <= 1L || ncol(z) <= 1L))
-  invisible(rapply(fnbody,find.nodes(patterns,here),how="list"))
+  invisible(rapply(fnbody,assign.nodenumbers(patterns,here),how="list"))
   body(filled.contour) <- as.call(c(head(fnbody,with(here,first-1)),
                                     insert,
                                     tail(fnbody,with(here,-(last-1)))))
   filled.contour
 }
 filled.contour.nolegend <- make.filled.contour.nolegend(graphics::filled.contour)
+
+
+make.newimageplot <- function() {
+  require(fields)
+  image.plot <- fields::image.plot
+  fnbody <- as.list(body(image.plot))
+  parsettings <- quote({
+    par(big.par)
+    par(plt = big.par$plt, xpd = TRUE)
+    par(mfg = mfg.save, new = FALSE)
+    invisible()
+  })
+  clipping <- quote(par(xpd=FALSE))
+  here <- new.env()
+  pattern <- list(replacement=alist(`if`,graphics.reset | add))
+  invisible(rapply(fnbody,assign.nodenumbers(pattern,here),how="list"))
+  fnbody[[get("replacement",,here)]] <- parsettings
+  body(image.plot) <- as.call(c(fnbody,clipping))
+  image.plot
+}
+
+newimageplot <- make.newimageplot()
+
